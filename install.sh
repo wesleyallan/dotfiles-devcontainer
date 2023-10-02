a93f1424d5688aa8f35e9cdf609d6a334deb9844
Wesley Silva <wesley.allansilva@gmail.com>
2023-10-02 01:49:02 -0300

    feat: Add startship config

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-LAST_DIR=$(basename $(pwd))
-
 mkdir -p /home/$USERNAME/.config
 ln -s /home/$USERNAME/$LAST_DIR/fish /home/$USERNAME/.config/
+
+if [ "$FULL" = "true" ]; then
+  ln -s /home/$USERNAME/$LAST_DIR/fish/config.fish.full /home/$USERNAME/.config/fish/config.fish
+  ln -s /home/$USERNAME/$LAST_DIR/starship.toml /home/$USERNAME/.config/starship.toml
+else
+  ln -s /home/$USERNAME/$LAST_DIR/fish/config.fish.soft /home/$USERNAME/.config/fish/config.fish
+fi
