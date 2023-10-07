5fa7fe678f2fd5313cad98a504fff018e935e1d0
Wesley Silva <wesley.allansilva@gmail.com>
2023-10-07 18:06:54 -0300

    feat: add check for execution

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$USERNAME" ] || [ -z "$LAST_DIR" ]; then
+  echo "As variáveis USERNAME e LAST_DIR devem ser definidas antes de executar este script."
+  exit 1
+fi
+
 mkdir -p /home/$USERNAME/.config
 ln -s /home/$USERNAME/$LAST_DIR/fish /home/$USERNAME/.config/
 
